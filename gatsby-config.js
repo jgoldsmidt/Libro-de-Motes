ca0ad4c8ac5917f4a9ce4af67874e75a315e0097
jgoldsmidt <julieta.gold@gmail.com>
2023-01-26 03:18:43 -0300

    upload template

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ const basePath = '/Libro-de-Motes'
 module.exports = {
     pathPrefix: basePath,
     siteMetadata: {
-        title: `Digital Publishing with Minimal Computing Project`,
-        description: `A template`,
-        author: `Course teachers`
+        title: `Libro de Motes de Damas y Caualleros`,
+        description: `Edición digital del Libro de Motes de Damas y Caualleros escrito por Luys de Milan`,
+        author: `Julieta Goldsmidt`
     },
     plugins: [
         `gatsby-plugin-material-ui`,
