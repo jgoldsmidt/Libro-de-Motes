c1c0354f5d000025dfec4c677aa2369d3323d6a5
jgoldsmidt <julieta.gold@gmail.com>
2023-01-26 02:40:00 -0300

    upload template

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+const basePath = '/Libro-de-Motes'
+
+module.exports = {
+    pathPrefix: basePath,
+    siteMetadata: {
+        title: `Digital Publishing with Minimal Computing Project`,
+        description: `A template`,
+        author: `Course teachers`
+    },
+    plugins: [
+        `gatsby-plugin-material-ui`,
+        `gatsby-theme-ceteicean`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `src/content/tei`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `src/content/pages`,
+                name: `html`,
+            },
+        },
+    ],
+}
